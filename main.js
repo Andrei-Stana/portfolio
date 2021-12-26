33410c315c2d8a6e842058dc53eae377ea8a1bf1
Andrei Stana <63104332+RoKyYy1412@users.noreply.github.com>
2021-12-26 02:04:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const intro = document.getElementById("intro");
+const mainSection = document.getElementById("main-section");
+const quote = document.getElementById("quote");
+
+
+quote.addEventListener("animationend", () => {     
+        intro.remove();
+        mainSection.style.display = "flex";
+})
+
+
+const elementName = document.getElementById("name");
+const description = document.getElementById("description");
+
+const menu = document.getElementById("menu");
+
+window.addEventListener("wheel", (e) => {
+    elementName.style.animation = "disappear 2s";
+    description.style.animation = "disappear 2s";
+
+    setTimeout(() => {     
+
+        mainSection.remove();
+    
+        menu.style.display = "flex";
+    }, 1900);
+})
